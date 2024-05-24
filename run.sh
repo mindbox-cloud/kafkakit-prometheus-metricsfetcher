2cd173150aae7ffe0f11046f692aaec3e96c9444
malyshevsergei <38503415+malyshevsergei@users.noreply.github.com>
2024-05-24 18:15:20 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ docker run kafka-metrics:latest \
   --broker-id-map "kafka-mailings-dm-stable-a-1-v1=1,kafka-mailings-dm-stable-a-2-v1=4,kafka-mailings-dm-stable-b-1-v1=2,kafka-mailings-dm-stable-b-2-v1=5,kafka-mailings-dm-stable-c-1-v1=3,kafka-mailings-dm-stable-c-2-v1=6" \
   --zk-addr 10.145.0.214:2181 \
   --prometheus-query-timeout 300s
+  
